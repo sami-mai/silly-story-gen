98887060292df86028ce34e02ae534b60c323f1e
EIT-sami-mai <samirah.maison@meltwater.org>
2018-09-07 08:14:29 +0000

    storestring of text inside a variable called storyText

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ var story = document.querySelector('.story');
 function randomValueFromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
+
+var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but he was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
