af2b06ec894e2954f958dbedbb5a40b217058481
EIT-sami-mai <samirah.maison@meltwater.org>
2018-09-07 08:19:44 +0000

    EVENT LISTENER AND PARTIAL FUNCTION DEFINITION

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ var storyText = 'It was 94 farenheit outside, so :insertx: went for a walk. When
 var insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 var insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 var insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  if (customName.value !== '') {
+    var name = customName.value;
+
+  }
+
+  if (document.getElementById("uk").checked) {
+    var weight = Math.round(300);
+    var temperature = Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
